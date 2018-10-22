4b832b1df6c6c3eefacf358869703b06a719fdad
quarkov <3dquarkov@gmail.com>
2018-10-22 17:04:17 +0200

    linux/windows wrapper implemented

--- core/ping.py
+++ core/ping.py
@@ -1,11 +1,12 @@
-from os import popen
+from os import popen, name
 
 
 def w_latency_time(hostname):
-    ping_report = [line.split() for line in popen("ping -n 10 " + hostname)]
+    ping_report = [line.split() for line in popen("ping -n 1 " + hostname)]
     return int(ping_report[-1][-2])
 
-
 def l_latency_time(hostname):
-    ping_report = [line.split() for line in popen("ping -c 10 -q " + hostname)]
+    ping_report = [line.split() for line in popen("ping -c 1 -q " + hostname)]
     return int(float(ping_report[-1][-2].split("/")[1]))
+
+latency_time = w_latency_time if name == "nt" else l_latency_time
