d34bfddf9568f6f675af139129d01e2ddda3f1e6
quarkov <3dquarkov@gmail.com>
2018-10-25 22:36:58 +0200

    hostname handling has been improved with socket module

--- core/inputs.py
+++ core/inputs.py
@@ -1,29 +1,16 @@
-from os import name, popen
+import socket
 
 
 def input_hostname():
-    hs = input("enter a hostname or press 'enter' to use default google.com: ")
-    hostname = hs if hs else "google.com"
-    if hostname.isdecimal():
-        print("incorrect hostname, try again")
-        return input_hostname()
+    hs = input("enter a hostname or press 'enter' to use default gdansk.pl: ")
+    hostname = hs if hs else "gdansk.pl"
 
-    response = [line.split() for line in popen("ping -n 1 " + hostname)] if name == "nt" else\
-        [line.split() for line in popen("ping -c 1 -q " + hostname)]
-    ip = 0
-    for chunk in response:
-        for piece in chunk:
-            if piece.count(".") == 3:
-                ip = piece
-                break
-        if ip:
-            break
-
-    if ip:
+    try:
+        socket.gethostbyname(hostname)
         print("hostname is set as", hostname)
         print()
         return hostname
-    else:
+    except socket.error:
         print("incorrect hostname, try again")
         print()
         return input_hostname()
@@ -40,6 +27,7 @@ def digit_input(min_value, default):
         print("an integer >=", min_value, "is expected, try again")
         return digit_input(min_value,default)
     return int(digit)
+
 
 def input_params():
     print("enter test duration in minutes (int >= 1) or press 'enter' to use default 1 minute:")
