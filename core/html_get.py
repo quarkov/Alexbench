3a0b45e07c6eb535f1ad93fb09ac78862963e413
quarkov <3dquarkov@gmail.com>
2018-10-28 18:47:54 +0100

    requests exceptions handle has been implemented

--- core/html_get.py
+++ core/html_get.py
@@ -4,7 +4,10 @@ import time as t
 
 def html_get(hostname):
     start = t.time()
-    page = requests.get("https://" + hostname)
-    finish = t.time()
-    code = page.status_code
-    return [int((finish - start)*1000), code] if code == 200 else [0, code]
+    try:
+        page = requests.get("http://" + hostname)
+        finish = t.time()
+        code = page.status_code
+        return [int((finish - start)*1000), code] if code == 200 else [0, code]
+    except requests.exceptions:
+        return [0, 0]
