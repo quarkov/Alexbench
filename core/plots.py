e072b560af8e0a7b6027242cad5c073390dd3df6
quarkov <3dquarkov@gmail.com>
2018-11-02 18:14:18 +0100

    rewritten using classes

--- core/plots.py
+++ core/plots.py
@@ -1,42 +1,50 @@
-import numpy as np
 from matplotlib import pyplot as plt
 
 
-def plot_init(filename, tests_number):
-    fig, ax = plt.subplots(figsize=(10, 0.5 * tests_number), frameon=False, tight_layout=True)
-    ax.set_title(filename, size=14)
-    ax.set_xlabel('time, ms')
-    ax.set_xlim(0, 2000)
-    ax.set_ylim(-1, tests_number)
-    ax.set_xticks(list(range(0, 2100, 100)), minor=True)
-    ax.xaxis.grid(True)
-    ax.invert_yaxis()
-    ind = np.arange(tests_number)
-    p = plt.barh(ind, 0, height=0.6, color='blue')
-    l = plt.barh(ind, 0, height=0.6, color='green')
-    plt.legend((p, l), ('loading time, ms', 'ping, ms'), loc=1)
-    plt.savefig(filename + ".svg")
-    return fig, ax
+class PlotWriter:
+    bar_limit = 12
+    x_min, x_max = 0, 2000
+    y_min, y_max = -1, bar_limit
 
-def plot_update(filename, fig, i, clock, loading, latency, status):
+    def __init__(self, filename):
+        self._filename = filename + ".svg"
+        self._fig, self._ax = plt.subplots(figsize=(10, 0.5 * self.bar_limit), frameon=False, tight_layout=True)
+        self.ax_init()
+        self._ax.set_yticks([])
+        plt.savefig(self._filename)
+        self._colorOk = ['blue', 'green']
+        self._colorError = ['red', 'black']
+        self._height = [0.6, 0.8]
+        self._coordMsg = [1400, 20]
 
-    if all([status == 200, loading <= 1950]):
-        plt.barh(clock, loading, color='blue', height=0.6)
-    if all([status == 200, loading > 1950]):
-        title = "status.code - " + str(status) + ", loading time - " + str(loading) + " ms"
-        plt.barh(i, 1950, color='red', height=0.6)
-        plt.text(1300, i, title, fontsize='small', va='baseline')
-    if status != 200:
-        title = "can't get web page, status.code - " + str(status)
-        plt.barh(i, loading, color='red', height=0.6)
-        plt.text(510, i, title, fontsize='small', va='baseline')
+    def ax_init(self):
+        self._ax.set_title(self._filename, size=14, loc='left')
+        self._ax.set_xlabel('time, ms')
+        self._ax.set_xlim(self.x_min, self.x_max)
+        self._ax.set_xticks(list(range(self.x_min, self.x_max + 50, 50)), minor=True)
+        self._ax.xaxis.grid(True)
+        self._ax.set_ylim(self.y_min, self.y_max)
+        self._ax.invert_yaxis()
 
-    if latency:
-        plt.barh(clock, latency, color='green', height=0.8)
-    else:
-        title = "response time is exceeded"
-        plt.barh(clock, 15, color='black', height=0.8)
-        plt.text(20, i, title, fontsize='small', va='baseline')
+    def ax_refresh(self):
+        self._ax.cla()
+        self.ax_init()
+        p = plt.barh(0, 0, height=0.6, color='blue')
+        l = plt.barh(0, 0, height=0.6, color='green')
+        plt.legend((p, l), ('loading time', 'latency'), loc=4)
 
-    fig.canvas.draw()
-    plt.savefig(filename + ".svg")
+    def update(self, monitor_data, probe, tests_count):
+        status_msg = "test " + str(probe + 1) + " has passed, tests remain: " + str(tests_count - probe - 1)
+        self.ax_refresh()
+        plt.text(self.x_max, self.y_min-0.5, status_msg, va='top', ha='right')
+        for record in monitor_data:
+            clock = record[0].value()
+            index = 0
+            for item in record[1:]:
+                color = self._colorOk[index] if not item.msg() else self._colorError[index]
+                plt.barh(clock, item.checked_value(), color=color, height=self._height[index])
+                plt.text(self._coordMsg[index], clock, item.msg(), fontsize='small', va='baseline')
+                index += 1
+
+        self._fig.canvas.draw()
+        plt.savefig(self._filename)
