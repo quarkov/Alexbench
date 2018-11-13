4537474c0059dea43f3a0df7d4a991c9d14919ad
quarkov <3dquarkov@gmail.com>
2018-11-13 13:31:07 +0100

    refactored

--- core/inputs.py
+++ core/inputs.py
@@ -35,7 +35,7 @@ def digit_input(min_value, max_value, default):
 def input_params():
     print("enter test duration in minutes (1440 >= int >= 1) or press 'enter' to use default 1 minute:")
     duration = digit_input(1, 1440, 1)
-    print("enter measurement frequency in seconds (",duration*60, ">= int >= 2) or press 'enter' to use default 5 seconds:")
-    frequency = digit_input(2, duration*60, 5)
-    print("ok, parameters are set; duration =", duration, "min, frequency =", frequency, "sec.")
-    return duration, frequency
+    print("enter measurement interval in seconds (",duration*60, ">= int >= 2) or press 'enter' to use default 5 seconds:")
+    interval = digit_input(2, duration*60, 5)
+    print("ok, parameters are set; duration =", duration, "min, interval =", interval, "sec.")
+    return duration, interval
